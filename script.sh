5dc8fb89693d1f6cd4b18afe7afdbd800be74b91
Chris Suran <christopher.suran@harness.io>
2025-07-11 16:59:30 -0400

    Add backend configuration for harness

--- script.sh
+++ script.sh
@@ -3,6 +3,9 @@
 git_branch="iacm-migration-$RANDOM"
 csv_file="$1"
 
+# Set TF password for backend to match HARNESS_PLATFORM_API_KEY already set by env file
+export TF_HTTP_PASSWORD="$HARNESS_PLATFORM_API_KEY"
+
 # Create Harness workspaces using CSV
 echo "Creating workspace in Harness..."
 tofu init
@@ -17,7 +20,11 @@ do
         # Skip csv header
         first_loop=false
     else
-        tf_path=$(echo $workspace | cut -d',' -f1)   
+        tf_path=$(echo $workspace | cut -d',' -f1)
+        workspace_id=$(echo $workspace | cut -d',' -f3)
+        account_id="$HARNESS_ACCOUNT_ID"
+        org_id=$(echo $workspace | cut -d',' -f4)
+        project_id=$(echo $workspace | cut -d',' -f5)
 
         echo "Processing $tf_path"
 
@@ -25,7 +32,7 @@ do
         cd $tf_path
 
         # Set safe directory to let git operate safely in this workspace
-        git config --global --add safe.directory "$(readlink -f $tf_path)"
+        git config --global --add safe.directory "$(git rev-parse --show-toplevel)"
 
         echo "Removing Backend configuration..."
 
@@ -42,9 +49,23 @@ do
                 regex=$(perl -0777 -pe "s/\s+/\\\E\\\s*\\\Q/igs" <(echo $regex_noslashes))
 
                 # Remove backend block match via regex (working around hcledit "hcledit block get terraform.backend" working, but not "hcledit block rm terraform.backend")
-                perl -0777 -pe "s/\Q$regex\E//igs" $tf
+                perl -0777 -i -pe "s/\Q$regex\E//igs" $tf
             fi
         done
+
+        # Create new backend configuration
+        cat >harness-backend.tf <<EOF
+terraform {
+  backend "http" {
+    address = "https://app.harness.io/gateway/iacm/api/orgs/$org_id/projects/$project_id/workspaces/$workspace_id/terraform-backend?accountIdentifier=$account_id"
+    username = "harness"
+    lock_address = "https://app.harness.io/gateway/iacm/api/orgs/$org_id/projects/$project_id/workspaces/$workspace_id/terraform-backend/lock?accountIdentifier=$account_id"
+    lock_method = "POST"
+    unlock_address = "https://app.harness.io/gateway/iacm/api/orgs/$org_id/projects/$project_id/workspaces/$workspace_id/terraform-backend/lock?accountIdentifier=$account_id"
+    unlock_method = "DELETE"
+    }
+}
+EOF
 
         # Migrate state
         echo "Migrating state..."
